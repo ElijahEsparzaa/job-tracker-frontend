0385f229547cd3f052be4b4fff62285446e43742
Robert Esparza <robertesparza8533@gmail.com>
2025-07-18 21:47:19 -0700

    Update JobList API URL to deployed backend

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import JobItem from './JobItem';
 
+const BASE_URL = 'https://job-tracker-backend-tqc2.onrender.com';
+
 export default function JobList() {
   const [jobs, setJobs] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/jobs')
-      .then(res => setJobs(res.data));
+    axios.get(`${BASE_URL}/api/jobs`)
+      .then(res => setJobs(res.data))
+      .catch(err => console.error('Error fetching jobs:', err));
   }, []);
 
   return (
